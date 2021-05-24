98891099e849d8e4714488a30d1d96e7e873c898
Alexander Krupka <bamaboy483@gmail.com>
2021-05-24 23:24:08 +0300

    +

---
+++ index.js
@@ -0,0 +1,51 @@
+const refs = {
+  body: document.querySelector("body"),
+  start: document.querySelector('button[data-action="start"]'),
+  stop: document.querySelector('button[data-action="stop"]'),
+  
+};
+
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const timer = {
+  switchColors : null,
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    this.isActive = true;
+    this.switchColors = setInterval(() => {
+      const min = 0;
+      const max = colors.length - 1;
+      const color = randomIntegerFromInterval(min, max);
+        refs.body.style.backgroundColor = colors[color];
+        
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.switchColors);
+    this.isActive = false;
+    },
+    
+};
+
+refs.start.addEventListener("click", () => {
+  timer.start() 
+});
+refs.stop.addEventListener("click",() => {
+  timer.stop() 
+});
+
